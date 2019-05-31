9e462f4c9e3998fe4a40ffa3319cf552858743d0
Darcie Howell <darcie.howell@aldautomotive.com>
2019-05-31 16:52:35 +0100

    initial commit

---
+++ TeamNames/Controllers/TeamMembersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamNames.Controllers
+{
+    //handles incoming HTTP requests and sends a response back to user
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Darcie : Person
+    {
+        public string FavouriteColour { get; set; }
+    }
+
+    public class TeamMembersController : ControllerBase
+    {
+        
+        //POST
+        
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+           
+            //take ID and Name entered
+            //add new MemberName 
+        }
+    }
+
+}
---
+++ TeamNames/Models/MemberName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamNames.Models
+{
+    public class MemberName
+    {
+        public int MemberId { get; set; }
+        public string Name { get; set; }
+    }
+}
