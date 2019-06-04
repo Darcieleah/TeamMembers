462202dd0d86b07280cf0330359143b60a39e106
Darcie Howell <darcie.howell@aldautomotive.com>
2019-06-04 16:37:58 +0100

    console print name submission

--- TeamNamesApi/TeamNames/Controllers/TeamMembersController.cs
+++ TeamNamesApi/TeamNames/Controllers/TeamMembersController.cs
@@ -10,7 +10,7 @@ namespace TeamNames.Controllers
     public class TeamMembersController : ControllerBase
     {
         //POST
-        
+        //api/teammembers
         [HttpPost]
         public void Post([FromBody] MemberNameRequest nameRequest)
         {
--- TeamNamesUI/site.js
+++ TeamNamesUI/site.js
@@ -3,18 +3,22 @@
 //console.log something when form submitted
 
 
+//takes in user input in name field
+var form = document.getElementById('name');
+var FD = new FormData(form);
+
+//this console logs the name passed in when you click save
+function consoleMessage(){
+    console.log(form.value);
+    };
+
+
+
+
+
 //XMLHTTPrequestobject created here? send request to web server
 //read response back from web server
 //Add Name function
 // onsubmit in HTML file performs add name function
 //use AJAX
 //send (string)
-
-function consoleMessage(){
-    console.log("name has been submitted");
-}
-
-
-// function addName(name){
-//     add name to database
-// }
