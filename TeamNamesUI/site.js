50ec49be22b96207600cb9d49582f293d9fcb30f
Darcie Howell <darcie.howell@aldautomotive.com>
2019-06-05 16:31:53 +0100

    amend controller

--- TeamNamesUI/site.js
+++ TeamNamesUI/site.js
@@ -4,17 +4,42 @@
 
 
 //takes in user input in name field
-var form = document.getElementById('name');
-var FD = new FormData(form);
-
-//this console logs the name passed in when you click save
-function consoleMessage(){
-    console.log(form.value);
-    };
+const form = document.getElementById('name');
+const FD = new FormData(form);
 
 
+// //this console logs the name passed in when you click save
+// function consoleMessage(){
+//     console.log(form.value);
+//     };
+
+// function postName(){
+//     $.ajax({
+//         type: "POST",
+//         url: ,
+//         contentType: "application/json",
+//         data: JSON.stringify(FD)s
+//     });
+//     console.log("post name function");
+// }
+
+function postName(){
+var data = JSON.stringify(FD);
+const Http = new XMLHttpRequest();
+const url='https://localhost:44366/api/teammembers';
+Http.open("POST", url);
+Http.setRequestHeader("Content-Type", "application/json");
+Http.setRequestHeader("Access-Control-Allow-Origin", "*")
+Http.send(data);
+console.log("post name function");
+}
 
 
+
+
+
+//convert text to json in order to POST
+//JSON.stringify()
 
 //XMLHTTPrequestobject created here? send request to web server
 //read response back from web server
