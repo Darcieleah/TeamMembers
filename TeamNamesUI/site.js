66eac4c6f5cca856c17dd1dfe0f7b89425ef00ec
Darcie Howell <darcie.howell@aldautomotive.com>
2019-07-08 12:01:33 +0100

    createandpopulate html tble with json get response

--- TeamNamesUI/site.js
+++ TeamNamesUI/site.js
@@ -26,45 +26,92 @@ function getNames(){
     request.onreadystatechange = () => {
         if (request.readyState == XMLHttpRequest.DONE) {
             console.log(request.response); 
-            generateTable(table, teamNames);
-            generateTableHead(table, data); 
+            let namesResponse = JSON.parse(request.response);
+            // generateTable(table, teamNames);
+            // generateTableHead(table, data); 
+            document.getElementById('tableGoesHere').innerHTML = json2table(namesResponse, 'table');   
         }
     }
     
     request.send(null);
 }
 
-let teamNames = [ 
-    { Name:"Darcie JS", ID: 1 },
-    { Name:"Jamie JS", ID: 2 },
-    { Name:"Gareth JS", ID: 3 },
-    { Name:"Dimitri JS", ID: 4 }
-];
+// let teamNames = [ 
+//     { Name:"Darcie JS", ID: 1 },
+//     { Name:"Jamie JS", ID: 2 },
+//     { Name:"Gareth JS", ID: 3 },
+//     { Name:"Dimitri JS", ID: 4 }
+// ];
 
-let table = document.querySelector("table");
-let data = Object.keys(teamNames[0]);
 
-function generateTableHead(table, data){
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
+
+// let table = document.querySelector("table");
+// let data = Object.keys(teamNames[0]);
+
+// function generateTableHead(table, data){
+//     let thead = table.createTHead();
+//     let row = thead.insertRow();
+//     for (let key of data) {
+//         let th = document.createElement("th");
+//         let text = document.createTextNode(key);
+//         th.appendChild(text);
+//         row.appendChild(th);
+//     }
+// }
+
+// function generateTable(table, data) {
+//     for (let element of data) {
+//       let row = table.insertRow();
+//       for (key in element) {
+//         let cell = row.insertCell();
+//         let text = document.createTextNode(element[key]);
+//         cell.appendChild(text);
+//       }
+//     }
+// }
+
+function json2table(json, classes) {
+  //1 table per property - each object in array has same properties of name and id
+  //this takes property names (keys) from first object and stores in cols
+    var cols = Object.keys(json[0]);
+    
+    var headerRow = '';
+    var bodyRows = '';
+
+    //if no class argument passed, then classes is empty string
+    
+    classes = classes || '';
+  
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
     }
-}
+  
+    cols.map(function(col) {
+      headerRow += '<th>' + capitalizeFirstLetter(col) + '</th>';
+    });
+  
+    json.map(function(row) {
+      bodyRows += '<tr>';
+  
+      cols.map(function(colName) {
+        bodyRows += '<td>' + row[colName] + '</td>';
+      })
+  
+      bodyRows += '</tr>';
+    });
+  
+    return '<table class="' +
+           classes +
+           '"><thead><tr>' +
+           headerRow +
+           '</tr></thead><tbody>' +
+           bodyRows +
+           '</tbody></table>';
+  }
 
-function generateTable(table, data) {
-    for (let element of data) {
-      let row = table.insertRow();
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      }
-    }
-}
+  
+
+
     
     
 
